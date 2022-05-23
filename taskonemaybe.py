409cd0f6d7b04c2685a23d42c5498d7ce5af8b85
LeifTeorin <61394735+LeifTeorin@users.noreply.github.com>
2022-05-23 16:46:44 +0200

    ahejhej

--- taskonemaybe.py
+++ taskonemaybe.py
@@ -16,6 +16,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import VotingClassifier
 from vecstack import stacking
 from sklearn.ensemble import StackingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
 
 from numpy import mean
 from numpy import std
@@ -27,7 +29,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 
 # importing machine learning models for prediction
-import xgboost as xgb
+#import xgboost as xgb
  
 # importing bagging module
 from sklearn.ensemble import BaggingRegressor
@@ -104,7 +106,25 @@ data2.columns = newcolumns
 
 #Fixing missing values
 #data.fillna(data.mean())
-data["joint3y"]=data2["joint3y"].fillna(data2["joint3y"].mean())
+def fill_class_mean(frame, colname):
+    frame[colname] = frame[colname].fillna(frame.groupby('posename')[colname].transform('mean')) 
+    
+fill_class_mean(data, "joint3y")
+fill_class_mean(data, "joint3z")
+fill_class_mean(data, "joint4x")
+fill_class_mean(data, "joint5z")
+fill_class_mean(data, "joint6x")
+fill_class_mean(data, "joint6y")
+
+
+fill_class_mean(data2, "joint3x")
+fill_class_mean(data2, "joint3y")
+fill_class_mean(data2, "joint3z")
+fill_class_mean(data2, "joint4z")
+fill_class_mean(data2, "joint5x")
+fill_class_mean(data2, "joint5y")
+
+""" data["joint3y"]=data2["joint3y"].fillna(data2["joint3y"].mean())
 data["joint3z"]=data2["joint3z"].fillna(data2["joint3z"].mean())
 data["joint4x"]=data2["joint4x"].fillna(data2["joint4x"].mean())
 data["joint5z"]=data2["joint5z"].fillna(data2["joint5z"].mean())
@@ -117,7 +137,7 @@ data2["joint3y"]=data2["joint3y"].fillna(data2["joint3y"].mean())
 data2["joint3z"]=data2["joint3z"].fillna(data2["joint3z"].mean())
 data2["joint4z"]=data2["joint4z"].fillna(data2["joint4z"].mean())
 data2["joint5x"]=data2["joint5x"].fillna(data2["joint5x"].mean())
-data2["joint5y"]=data2["joint5y"].fillna(data2["joint5y"].mean())
+data2["joint5y"]=data2["joint5y"].fillna(data2["joint5y"].mean()) """
 
 newdata = data[data.columns[:240]]
 #newdata = pd.DataFrame()
@@ -257,7 +277,7 @@ def svmClass():
 
 def forestClass():
     #Create a Gaussian Classifier
-    clf=RandomForestClassifier(n_estimators=100)
+    clf=RandomForestClassifier(n_estimators=1000, max_depth=120)
 
     #Train the model using the training sets y_pred=clf.predict(X_test)
     clf.fit(X_train,Y_train)
@@ -288,9 +308,14 @@ def mlpClass():
 def ensembleClass1():
     model_1 = MLPClassifier(hidden_layer_sizes=(200,150,100,50), max_iter=600,activation = 'relu',solver='adam',random_state=1)
     model_2 = svm.SVC(kernel='linear')
-    model_3 = RandomForestClassifier(n_estimators=300)
+    model_3 = RandomForestClassifier(n_estimators=1000)
+    model_4 = KNeighborsClassifier(n_neighbors = 2)
+    model_5 = RandomForestClassifier(n_estimators=1000)
+    model_6 = DecisionTreeClassifier(criterion="entropy", max_depth=120)
+    model_7 = LogisticRegression(max_iter = 600)
+    model_8 = svm.SVC(kernel='linear')
     final_model = VotingClassifier(
-    estimators=[('mlp', model_1), ('svm', model_2), ('rf', model_3)], voting='hard')
+    estimators=[('mlp', model_1), ('svm', model_2), ('rf', model_3), ('knn', model_4), ('rf2', model_5), ('dt', model_6), ('lr', model_7)], voting='hard')
  
     # training all the model on the train dataset
     final_model.fit(X_train, Y_train)
@@ -298,9 +323,9 @@ def ensembleClass1():
     # predicting the output on the test dataset
     pred_final = final_model.predict(X_test)
     print("Accuracy for voting:",metrics.accuracy_score(Y_test, pred_final))
-
+    
 def ensembleClass2():
-    model_1 = KNeighborsClassifier(n_neighbors=2)
+    model_1 = KNeighborsClassifier(n_neighbors=1)
     model_2 = svm.SVC(kernel='linear')
     model_3 = DecisionTreeClassifier(criterion="entropy", max_depth=120)
     final_model = VotingClassifier(
@@ -311,31 +336,40 @@ def ensembleClass2():
  
     # predicting the output on the test dataset
     pred_final = final_model.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(Y_test, pred_final))
+    print("Accuracy for voting 2:",metrics.accuracy_score(Y_test, pred_final))
 
 def ensembleStack():
     level0 = list()
-    level0.append(('lr', LogisticRegression()))
-    level0.append(('knn', KNeighborsClassifier(n_neighbors=1)))
+#    level0.append(('lr', LogisticRegression(max_iter = 1000)))
+    level0.append(('knn', KNeighborsClassifier(n_neighbors=2)))
     level0.append(('svm', svm.SVC(kernel='linear')))
-    level0.append(('rf', RandomForestClassifier(n_estimators=100)))
+    level0.append(('rf', RandomForestClassifier(n_estimators=1000)))
+    level0.append(('mlp', MLPClassifier(hidden_layer_sizes=(200,150,100,50), max_iter=600,activation = 'relu',solver='adam',random_state=1)))
+    level0.append(('dt', DecisionTreeClassifier(max_depth = 120)))
 
-    level1 = RandomForestClassifier(n_estimators=100)
+    level1 = RandomForestClassifier(n_estimators = 1000)
 
     model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
     model.fit(X_train, Y_train)
     pred = model.predict(X_test)
     print("Accuracy for stack:",metrics.accuracy_score(Y_test, pred))
+    
+def boosterClass():
+    model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=200, max_depth = 100)
+    model.fit(X_train, Y_train)
+    pred = model.predict(X_test)
+    print("Accuracy for booster:",metrics.accuracy_score(Y_test, pred))
+    
 
 def ensembleBagging():
-    model = BaggingRegressor(base_estimator=xgb.XGBRegressor())
+    model = BaggingClassifier(base_estimator=RandomForestClassifier(n_estimators=100), n_estimators=10, random_state=0)
  
     # training model
-    model.fit(X_train, Y2_train)
+    model.fit(X_train, Y_train)
     
     # predicting the output on the test dataset
     pred = model.predict(X_test)
-    print("Accuracy for bagging:",metrics.accuracy_score(Y2_test, pred))
+    print("Accuracy for bagging:",metrics.accuracy_score(Y_test, pred)) 
 
 def evaluate_model(model, X, y):
 	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
@@ -359,4 +393,12 @@ def plotPerformance():
     plt.boxplot(results, labels=names, showmeans=True)
     plt.show()
 
-ensembleStack()
+
+#forestClass()
+#ensembleClass1()
+#ensembleClass2()
+#ensembleStack() 
+#boosterClass()
+#ensembleBagging()
+
+kNNClass()
